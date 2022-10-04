b705816e6c5edaec50e9e20206d155bc142e8f51
Dhruv Choubey <Choubeydhruv2014@gmail.com>
2022-10-04 22:24:43 +0530

    Card Components Made

---
+++ src/Components/headlineCard/card.jsx
@@ -0,0 +1,96 @@
+export default function Card(){
+    return(
+        <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6 ">
+            {/*Card*/}
+            <div className="rounded-xl relative w-[350px]">
+                {/*Overlay*/}
+                <div className="absolute w-full h-full  bg-black/50 rounded-xl text-white">
+                  <p className="text-2xl font-bold px-2 pt-2">Sun's Out, BOGO Out</p>
+                  <p className="px-2 ">Through 8/26</p>
+                  <button className="border-white bg-white mx-2 absolute bottom-4 text-black">Order Now</button>
+                </div>
+                <img
+                    className="max-h-[1640px] md:max-h-[200px] w-full object-fit rounded-xl"
+                    src="https://images.pexels.com/photos/2474661/pexels-photo-2474661.jpeg?auto=compress&cs=tinysrgb&w=600"
+                    alt="/">
+
+                </img>
+            </div>
+
+            <div className="rounded-xl relative w-[350px]">
+                {/*Overlay*/}
+                <div className="absolute w-full h-full  bg-black/50 rounded-xl text-white">
+                    <p className="text-2xl font-bold px-2 pt-2">Sun's Out, BOGO Out</p>
+                    <p className="px-2 ">Through 8/26</p>
+                    <button className="border-white bg-white mx-2 absolute bottom-4 text-black">Order Now</button>
+                </div>
+                <img
+                    className="max-h-[1640px] md:max-h-[200px] w-full object-fit rounded-xl"
+                    src="https://images.unsplash.com/photo-1606491956689-2ea866880c84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGluZGlhbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+                    alt="/">
+
+                </img>
+            </div>
+
+            <div className="rounded-xl relative w-[350px]">
+                {/*Overlay*/}
+                <div className="absolute w-full h-full  bg-black/50 rounded-xl text-white">
+                    <p className="text-2xl font-bold px-2 pt-2">Sun's Out, BOGO Out</p>
+                    <p className="px-2 ">Through 8/26</p>
+                    <button className="border-white bg-white mx-2 absolute bottom-4 text-black">Order Now</button>
+                </div>
+                <img
+                    className="max-h-[1640px] md:max-h-[200px] w-full object-fit rounded-xl"
+                    src="https://media.istockphoto.com/photos/shahi-paneer-or-paneer-kadai-picture-id1305516603?k=20&m=1305516603&s=612x612&w=0&h=9z7YhLKh9CflB1tervIRpx_u06Ybfttf_8zfCqV1C9w="
+                    alt="/">
+
+                </img>
+            </div>
+
+            <div className="rounded-xl relative w-[350px]">
+                {/*Overlay*/}
+                <div className="absolute w-full h-full  bg-black/50 rounded-xl text-white">
+                    <p className="text-2xl font-bold px-2 pt-2">Sun's Out, BOGO Out</p>
+                    <p className="px-2 ">Through 8/26</p>
+                    <button className="border-white bg-white mx-2 absolute bottom-4 text-black">Order Now</button>
+                </div>
+                <img
+                    className="max-h-[1640px] md:max-h-[200px] w-full object-fit rounded-xl"
+                    src="https://media.istockphoto.com/photos/fish-biryani-with-basmati-rice-indian-food-picture-id488481490?k=20&m=488481490&s=612x612&w=0&h=HYP2KxiC1e2tAtzmfrA7xxs3u8LD1wjSLPUD9bZ48eU="
+                    alt="/">
+
+                </img>
+            </div>
+
+            <div className="rounded-xl relative w-[350px]">
+                {/*Overlay*/}
+                <div className="absolute w-full h-full  bg-black/50 rounded-xl text-white">
+                    <p className="text-2xl font-bold px-2 pt-2">Sun's Out, BOGO Out</p>
+                    <p className="px-2 ">Through 8/26</p>
+                    <button className="border-white bg-white mx-2 absolute bottom-4 text-black">Order Now</button>
+                </div>
+                <img
+                    className="max-h-[1640px] md:max-h-[200px] w-full object-fit rounded-xl"
+                    src="https://media.istockphoto.com/photos/malai-kofta-curry-picture-id1397757522?k=20&m=1397757522&s=612x612&w=0&h=cy6mSuEKCC_sM0R5zvnMPeBfJlV2d2GtjfTnxzt6awU="
+                    alt="/">
+
+                </img>
+            </div>
+
+            <div className="rounded-xl relative w-[350px]">
+                {/*Overlay*/}
+                <div className="absolute w-full h-full  bg-black/50 rounded-xl text-white">
+                    <p className="text-2xl font-bold px-2 pt-2">Sun's Out, BOGO Out</p>
+                    <p className="px-2 ">Through 8/26</p>
+                    <button className="border-white bg-white mx-2 absolute bottom-4 text-black">Order Now</button>
+                </div>
+                <img
+                    className="max-h-[1640px] md:max-h-[200px] w-full object-fit rounded-xl"
+                    src="https://media.istockphoto.com/photos/traditional-indian-vegetarian-biryani-veg-biryani-recipe-picture-id1224684173?k=20&m=1224684173&s=612x612&w=0&h=6EUz8CRmUAQ10FGeAcEKap3sdKsSLn4_7KReZtaLEi0="
+                    alt="/">
+
+                </img>
+            </div>
+        </div>
+    );
+}
