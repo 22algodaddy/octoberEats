b4868e140c65c178127c1b1f679d89b03543d303
Dhruv Choubey <Choubeydhruv2014@gmail.com>
2022-10-08 17:23:29 +0530

    Project Competed, Waiting for Quality Suggestions

--- src/Components/headlineCard/card.jsx
+++ src/Components/headlineCard/card.jsx
@@ -20,8 +20,8 @@ export default function Card(){
             <div className="rounded-xl relative w-[350px]">
                 {/*Overlay*/}
                 <div className="absolute w-full h-full  bg-black/50 rounded-xl text-white">
-                    <p className="text-2xl font-bold px-2 pt-2">Sun's Out, BOGO Out</p>
-                    <p className="px-2 ">Through 8/26</p>
+                    <p className="text-2xl font-bold px-2 pt-2">New Restaurants</p>
+                    <p className="px-2 ">Added Daily</p>
                     <button className="border-white bg-white mx-2 absolute bottom-4 text-black">Order Now</button>
                 </div>
                 <img
@@ -35,60 +35,14 @@ export default function Card(){
             <div className="rounded-xl relative w-[350px]">
                 {/*Overlay*/}
                 <div className="absolute w-full h-full  bg-black/50 rounded-xl text-white">
-                    <p className="text-2xl font-bold px-2 pt-2">Sun's Out, BOGO Out</p>
-                    <p className="px-2 ">Through 8/26</p>
+                    <p className="text-2xl font-bold px-2 pt-2">Order for Special Occasion</p>
+                    <p className="px-2 ">Also</p>
                     <button className="border-white bg-white mx-2 absolute bottom-4 text-black">Order Now</button>
                 </div>
                 <img
                     className="max-h-[1640px] md:max-h-[200px] w-full object-fit rounded-xl"
                     src="https://media.istockphoto.com/photos/shahi-paneer-or-paneer-kadai-picture-id1305516603?k=20&m=1305516603&s=612x612&w=0&h=9z7YhLKh9CflB1tervIRpx_u06Ybfttf_8zfCqV1C9w="
                     alt="/">
-
-                </img>
-            </div>
-
-            <div className="rounded-xl relative w-[350px]">
-                {/*Overlay*/}
-                <div className="absolute w-full h-full  bg-black/50 rounded-xl text-white">
-                    <p className="text-2xl font-bold px-2 pt-2">Sun's Out, BOGO Out</p>
-                    <p className="px-2 ">Through 8/26</p>
-                    <button className="border-white bg-white mx-2 absolute bottom-4 text-black">Order Now</button>
-                </div>
-                <img
-                    className="max-h-[1640px] md:max-h-[200px] w-full object-fit rounded-xl"
-                    src="https://media.istockphoto.com/photos/fish-biryani-with-basmati-rice-indian-food-picture-id488481490?k=20&m=488481490&s=612x612&w=0&h=HYP2KxiC1e2tAtzmfrA7xxs3u8LD1wjSLPUD9bZ48eU="
-                    alt="/">
-
-                </img>
-            </div>
-
-            <div className="rounded-xl relative w-[350px]">
-                {/*Overlay*/}
-                <div className="absolute w-full h-full  bg-black/50 rounded-xl text-white">
-                    <p className="text-2xl font-bold px-2 pt-2">Sun's Out, BOGO Out</p>
-                    <p className="px-2 ">Through 8/26</p>
-                    <button className="border-white bg-white mx-2 absolute bottom-4 text-black">Order Now</button>
-                </div>
-                <img
-                    className="max-h-[1640px] md:max-h-[200px] w-full object-fit rounded-xl"
-                    src="https://media.istockphoto.com/photos/malai-kofta-curry-picture-id1397757522?k=20&m=1397757522&s=612x612&w=0&h=cy6mSuEKCC_sM0R5zvnMPeBfJlV2d2GtjfTnxzt6awU="
-                    alt="/">
-
-                </img>
-            </div>
-
-            <div className="rounded-xl relative w-[350px]">
-                {/*Overlay*/}
-                <div className="absolute w-full h-full  bg-black/50 rounded-xl text-white">
-                    <p className="text-2xl font-bold px-2 pt-2">Sun's Out, BOGO Out</p>
-                    <p className="px-2 ">Through 8/26</p>
-                    <button className="border-white bg-white mx-2 absolute bottom-4 text-black">Order Now</button>
-                </div>
-                <img
-                    className="max-h-[1640px] md:max-h-[200px] w-full object-fit rounded-xl"
-                    src="https://media.istockphoto.com/photos/traditional-indian-vegetarian-biryani-veg-biryani-recipe-picture-id1224684173?k=20&m=1224684173&s=612x612&w=0&h=6EUz8CRmUAQ10FGeAcEKap3sdKsSLn4_7KReZtaLEi0="
-                    alt="/">
-
                 </img>
             </div>
         </div>
